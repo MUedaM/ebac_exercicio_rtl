a7d469adc50c96ad83d588cbf27172f889cf6833
MUedaM <mi-ueda@outlook.com>
2026-01-27 01:26:43 -0300

    corrigindo imageurl e finalizando teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  it("Deve ser adicionado 2 comentarios", () => {
+    render(<PostComment />);
+
+    fireEvent.change(screen.getByTestId("comentario-textarea"), {
+      target: {
+        value: "Primeiro comentario adicionado automaticamente",
+      },
     });
+    fireEvent.click(screen.getByTestId("comentario-postar-button"));
+
+    fireEvent.change(screen.getByTestId("comentario-textarea"), {
+      target: {
+        value: "Segundo comentario adicionado automaticamente",
+      },
+    });
+    fireEvent.click(screen.getByTestId("comentario-postar-button"));
+
+    expect(screen.getAllByTestId("comentario-list")).toHaveLength(2);
+  });
 });
